0665148c4a3c3656b9a4fed3e5b0fba0d0650da5
Hung Duong Quang <qhungg289@protonmail.com>
2021-04-18 22:49:17 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,55 @@
-const container = document.getElementById('container');
-const gridItem = document.getElementsByClassName('grid-item');
+const container = document.getElementById("grid-container");
+const gridItem = document.getElementsByClassName("grid-item");
+const clearButton = document.getElementById("clear-button");
+const sizeButton = document.getElementById("grid-size");
 
-function makeRows(rows, cols){
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i++){
-        let cell = document.createElement('div');
-        container.appendChild(cell).className = 'grid-item';
+window.addEventListener("load", setDefaultGrid);
+clearButton.addEventListener("click", function() {
+    for (let item of gridItem) {
+        item.style.backgroundColor = null;
+    }
+});
+sizeButton.addEventListener("click", changeSize);
+
+function setDefaultGrid() {
+    setGridSize(8);
+    makeGrid(8);
+}
+
+function setGridSize(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+}
+
+function makeGrid(size) {
+	for (i = 0; i < size * size; i++) {
+		let cell = document.createElement("div");
+        cell.addEventListener("mouseover", function(){changeBgColor(cell)});
+		container.appendChild(cell).className = "grid-item";
+	}
+}
+
+function changeSize() {
+    let nSize = prompt("Enter new grid size!");
+    if (nSize != null) {
+        nSize = +(nSize);
+        if (nSize < 1 || nSize > 64 || Number.isNaN(nSize)) {
+            alert("Enter a valid number from 1 - 64!");
+            changeSize();
+        } else {
+            removeGridElement();
+            setGridSize(nSize);
+            makeGrid(nSize);
+        }
     }
 }
 
-makeRows(8, 8);
-
-function changeState(elem){
-    elem.classList.add('hover');
+function changeBgColor(elem) {
+    elem.style.backgroundColor = "aquamarine";
 }
 
-for (let item of gridItem){
-    item.addEventListener("mouseover", function() {
-        changeState(item);
-    })
-    item.addEventListener("mouseout", event => {
-        console.log('Mouse out');
+function removeGridElement() {
+    const gridArr = Array.from(container.childNodes);
+    gridArr.forEach((elem) => {
+        container.removeChild(elem);
     })
 }
